1859d18e55c5a5e6e848ada1a3cafbd7e6031a51
Usuario <Usurio@DESKTOP-KLN9T82>
2022-09-03 15:45:18 -0300

    deploy necessario

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -3,7 +3,6 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
         host: 'ec2-44-205-64-253.compute-1.amazonaws.com',
-        port: 5432,
         user: 'ipvskfulzrjukh',
         password: '314ea5dc6ba44a3125d2ceb34c29dc18b93659feb6b4dfb128cba828e835266a',
         database: 'detkc1hier5k83'
