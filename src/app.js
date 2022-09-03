1684f29da736aacf843db56b50861abf192430d0
Usuario <Usurio@DESKTOP-KLN9T82>
2022-09-03 14:44:08 -0300

    deploy necessario

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const express = require('express')
 const app = express()
 const router = require('./router')
 const cors = require('cors')
+
 app.use(express.json({
     limit: '5mb'
 }))
+
 app.use(cors())
 app.use(router)
 
