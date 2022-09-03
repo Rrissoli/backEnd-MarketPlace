c6364a9001abec4d8a4c71d36e71890050cbbf83
Usuario <Usurio@DESKTOP-KLN9T82>
2022-09-03 15:19:29 -0300

    deploy necessario

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -3,12 +3,13 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
         host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE
     },
     ssl: {
-        rejectUnathorized: false
+        rejectUnauthorized: false
     }
 });
 module.exports = knex
