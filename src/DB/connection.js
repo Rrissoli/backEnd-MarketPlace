61e1b49a029aadb2a467dcb805a7db393e3ad2f1
Usuario <Usurio@DESKTOP-KLN9T82>
2022-09-03 15:50:30 -0300

    deploy necessario

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -2,13 +2,14 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'ec2-44-205-64-253.compute-1.amazonaws.com',
-        user: 'ipvskfulzrjukh',
-        password: '314ea5dc6ba44a3125d2ceb34c29dc18b93659feb6b4dfb128cba828e835266a',
-        database: 'detkc1hier5k83',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         ssl: {
             rejectUnauthorized: false
         }
-    });
-
+    }
+});
 module.exports = knex
