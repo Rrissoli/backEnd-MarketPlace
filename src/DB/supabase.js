ffcc029cd3f97ddbe3506143527d7818dc469302
Usuario <Usurio@DESKTOP-KLN9T82>
2022-09-05 12:52:24 -0300

    deploy necessario

--- src/DB/supabase.js
+++ src/DB/supabase.js
@@ -1,3 +1,3 @@
 const { createClient } = require('@supabase/supabase-js')
-const supabase = createClient('https://wiwjwflfqzynoqlpgkcj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indpd2p3ZmxmcXp5bm9xbHBna2NqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2MTc0NjQwNCwiZXhwIjoxOTc3MzIyNDA0fQ.NGkmLXtqNnoLC8Cxpxg322ghIJefX4y9eCJOgnil0T0')
+const supabase = createClient('https://wiwjwflfqzynoqlpgkcj.supabase.co', process.env.DB_SUPABASE)
 module.exports = supabase;
